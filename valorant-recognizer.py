f761c0ecd7dd00f11135845b8ae51d74ed295c47
Sodment <sodment@gmail.com>
2021-11-17 20:49:47 +0100

    Inital Commit

---
+++ valorant-recognizer.py
@@ -0,0 +1,122 @@
+import cv2
+import glob
+import numpy
+import random
+import numpy as np # for transformation
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import matplotlib.pyplot as plt
+
+from torch.utils.data import Dataset, DataLoader
+
+def flatten(t):
+    return [item for sublist in t for item in sublist]
+
+#######################################################
+#               Define Transforms
+#######################################################
+
+#To define an augmentation pipeline, you need to create an instance of the Compose class.
+#As an argument to the Compose class, you need to pass a list of augmentations you want to apply. 
+#A call to Compose will return a transform function that will perform image augmentation.
+#(https://albumentations.ai/docs/getting_started/image_augmentation/)
+
+train_transforms = A.Compose(
+    [
+        A.SmallestMaxSize(max_size=350),
+        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=360, p=0.5),
+        A.RandomCrop(height=256, width=256),
+        A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+        A.RandomBrightnessContrast(p=0.5),
+        A.MultiplicativeNoise(multiplier=[0.5,2], per_channel=True, p=0.2),
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
+        A.RandomBrightnessContrast(brightness_limit=(-0.1,0.1), contrast_limit=(-0.1, 0.1), p=0.5),
+        ToTensorV2(),
+    ]
+)
+
+test_transforms = A.Compose(
+    [
+        A.SmallestMaxSize(max_size=350),
+        A.CenterCrop(height=256, width=256),
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),
+    ]
+)
+
+####################################################
+#       Create Train, Valid and Test sets
+####################################################
+train_data_path = 'images/Train' 
+test_data_path = 'images/Test'
+
+train_image_paths = [] #to store image paths in list
+classes = [] #to store class values
+
+#1.
+# get all the paths from train_data_path and append image paths and class to to respective lists
+# eg. train path-> 'images/train/26.Pont_du_Gard/4321ee6695c23c7b.jpg'
+# eg. class -> 26.Pont_du_Gard
+for data_path in glob.glob(train_data_path + '/*'):
+    classes.append(data_path.split('/')[-1]) 
+    train_image_paths.append(glob.glob(data_path + '/*'))
+train_image_paths = list(flatten(train_image_paths))
+random.shuffle(train_image_paths)
+
+print('train_image_path example: ', train_image_paths[0])
+print('class example: ', classes[0])
+
+train_image_paths, valid_image_paths = train_image_paths[:int(0.8*len(train_image_paths))], train_image_paths[int(0.8*len(train_image_paths)):]
+
+#3.
+# create the test_image_paths
+test_image_paths = []
+for data_path in glob.glob(test_data_path + '/*'):
+    test_image_paths.append(glob.glob(data_path + '/*'))
+
+test_image_paths = list(flatten(test_image_paths))
+
+print("Train size: {}\nValid size: {}\nTest size: {}".format(len(train_image_paths), len(valid_image_paths), len(test_image_paths)))
+
+#######################################################
+#      Create dictionary for class indexes
+#######################################################
+
+idx_to_class = {i:j for i, j in enumerate(classes)}
+class_to_idx = {value:key for key,value in idx_to_class.items()}
+
+#######################################################
+#               Define Dataset Class
+#######################################################
+
+class AgentDataset(Dataset):
+    def __init__(self, image_paths, transform=False):
+        self.image_paths = image_paths
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_filepath = self.image_paths[idx]
+        image = cv2.imread(image_filepath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        label = image_filepath.split('/')[-2]
+        label = class_to_idx[label]
+        if self.transform is not None:
+            image = self.transform(image=image)["image"]
+        
+        return image, label
+    
+#######################################################
+#                  Create Dataset
+#######################################################
+
+train_dataset = AgentDataset(train_image_paths,train_transforms)
+valid_dataset = AgentDataset(valid_image_paths,test_transforms) #test transforms are applied
+test_dataset = AgentDataset(test_image_paths,test_transforms)
+
+print('The shape of tensor for 50th image in train dataset: ',train_dataset[0][0].shape)
+print('The label for 50th image in train dataset: ',train_dataset[0][0])
