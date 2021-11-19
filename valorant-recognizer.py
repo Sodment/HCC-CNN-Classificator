99ed313122c02450628564d462023ede778249df
Sodment <sodment@gmail.com>
2021-11-19 17:29:25 +0100

    Test3

--- valorant-recognizer.py
+++ valorant-recognizer.py
@@ -5,6 +5,8 @@ import numpy as np # for transformation
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import matplotlib.pyplot as plt
+#ez
+
 
 from torch.utils.data import Dataset, DataLoader
 
